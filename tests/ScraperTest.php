c8e1a5ec10e7603e47897cf1fcb3635a0f09ffbf
adidude <menon.or.adithya@gmail.com>
2021-09-13 23:34:51 +0100

    Added multiple assertions on price fetching

--- tests/ScraperTest.php
+++ tests/ScraperTest.php
@@ -8,14 +8,35 @@ use Goutte\Client;
 
 class ScraperTest extends TestCase
 {   
-    public function testPriceFetch()
+    public function setUp() : void
     {
         $client = new Client();
-        $crawler = $client->request("GET","https://videx.comesconnected.com");
+        $this->crawler = $client->request("GET","https://videx.comesconnected.com");
+        
+        $this->scraper = new Scraper();
+    }
+
+    public function tearDown() : void
+    {
+        unset($this->crawler);
+        unset($this->scraper);
+    }
+
+    public function testPriceFetch()
+    {
         $answer = [72,120,192,66,108,174];
-        $scraper = new Scraper();
-        $compute = $scraper->getPrices($crawler);
-        $this->assertEquals($answer, $compute, "The annual prices are [72,120,192,66,108,174]");
+        $output = $this->scraper->getPrices($this->crawler);
+        $this->assertNotNull($output);
+        $this->assertCount(6, $output, "Expected 6 packages");
+        $this->assertContainsOnly("int",$output,"Expecting to recieve array with integers");
+        
+        #Check if all prices are positive
+        for ($i=0; $i < 6; $i++) { 
+            $this->assertGreaterThan(0,$output[$i]);
+        }
+
+        #Potentially unwanted test
+        $this->assertEquals($answer, $output, "The annual prices are [72,120,192,66,108,174] As long as prices have not changed");
     }
 }
 ?>
